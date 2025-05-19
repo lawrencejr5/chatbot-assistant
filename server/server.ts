4f361e9ba2b7f5bd9eb6d16c1d39c791b7f6c17d
lawrencejr5 <oputalawrence@gmail.com>
2025-05-19 15:42:46 +0100

    no commit

--- server/server.ts
+++ server/server.ts
@@ -2,29 +2,18 @@ require("dotenv").config();
 
 import express, { Request, Response } from "express";
 
-import axios from "axios";
+// import
 
 const app = express();
 const router = express.Router();
 
 app.use(express.json());
 
-const model = process.env.HF_MODEL;
-const token = process.env.HF_TOKEN;
-
-const hf_url = `https://api-inference.huggingface.co/models/${model}`;
-
 router.post("/", async (req: Request, res: Response): Promise<void> => {
   try {
     const { input } = req.body;
 
-    const { data } = await axios.post(
-      hf_url,
-      { inputs: input, parameters: { temperature: 0.7, max_new_tokens: 100 } },
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
-
-    res.status(200).json({ msg: "success", response: data[0]?.generated_text });
+    res.status(200).json({ msg: "success", response: input });
   } catch (error) {
     console.log(error);
   }
